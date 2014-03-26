e1794b905222765b6a52fa2ab4cb2cfbcd0034e3
Han Xu <hgcrpd@gmail.com>
2014-03-26 09:36:14 -0700

    taskList directive

--- app.js
+++ app.js
@@ -6,23 +6,36 @@ angular.module('MyApp').
   controller('TaskCtrl', ['$scope', function($scope) {
     $scope.welcomeMessage = "Hello world!";
 
-    $scope.taskList = [
-      {name: "My first task", completed: false},
-      {name: "Second task", completed: true},
-      {name: "Third task", completed: false}
-    ];
-
-    $scope.addTask = function() {
-      if (!($scope.newTaskName === undefined || $scope.newTaskName === '')) {
-        $scope.taskList.push(
-          {name: $scope.newTaskName, completed: false}
-        );
-        delete $scope.newTaskName;
-      }
-    };
-
-    $scope.removeTask = function(taskIdx) {
-      $scope.taskList.splice(taskIdx, 1);
-    };
 
   }]);
+
+
+angular.module('MyApp').
+  directive('taskList', function() {
+    return {
+      scope: true,
+      controller: ['$scope', function($scope) {
+
+        $scope.taskList = [
+          {name: "My first task", completed: false},
+          {name: "Second task", completed: true},
+          {name: "Third task", completed: false}
+        ];
+
+        $scope.addTask = function() {
+          if (!($scope.newTaskName === undefined || $scope.newTaskName === '')) {
+            $scope.taskList.push(
+              {name: $scope.newTaskName, completed: false}
+            );
+            delete $scope.newTaskName;
+          }
+        };
+
+        $scope.removeTask = function(taskIdx) {
+          $scope.taskList.splice(taskIdx, 1);
+        };
+
+      }],
+      templateUrl: 'task-list.html'
+    };
+  });
