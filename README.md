b48246da99a4d767e89dcf610420ba011a6f35f3
Han Xu <hgcrpd@gmail.com>
2014-03-27 22:23:24 -0700

    completed

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ wids2014-angular
 ================
 
 World Internet Developer Summit 2014 - Hands on Introduction to AngularJS
+
+
+Features to implement
+=====================
+
+  - Show a list of tasks        Views / Expressions / Interpolation
+  - Toggle task completion      Two way databinding
+  - Add tasks                   Controllers
+  - Remove tasks
+  - Filter tasks
+  - Click to edit
+  - Multiple instances          Directives
+  - Saving to backend           Services
--- app.js
+++ app.js
@@ -1,3 +1,50 @@
 'use strict';
 
 angular.module('MyApp', []);
+
+angular.module('MyApp').
+  controller('TodoCtrl', ['$scope', 'MyBackend', function($scope, MyBackend) {
+    $scope.helloMessage = "Hi everyone";
+
+
+  }]);
+
+angular.module('MyApp').
+  directive('taskList', function() {
+    return {
+      scope: true,
+      controller: function($scope, $attrs) {
+        $scope.taskListName = $attrs.listName || 'My tasks';
+        $scope.taskList = [
+          {name: "My first task", completed: true},
+          {name: "My second task", completed: false},
+          {name: "Third task", completed: false}
+        ];
+        $scope.addTask = function() {
+          if (!($scope.newTaskName === undefined || $scope.newTaskName === '')) {
+            $scope.taskList.push({name: $scope.newTaskName, completed: false});
+            $scope.newTaskName = undefined;
+          }
+        };
+
+        $scope.removeTask = function(idx) {
+          $scope.taskList.splice(idx, 1);
+        };
+
+      },
+      templateUrl: 'task-list.html'
+    }
+  });
+
+
+angular.module('MyApp').
+  factory('MyBackend', function() {
+    var svc = {};
+
+    svc.getTaskList = function() {
+
+    };
+
+    return svc;
+
+  });
